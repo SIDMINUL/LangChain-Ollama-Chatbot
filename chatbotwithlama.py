aa53a8336b90c8b2369762ee6a4184e136ac7aea
Abdul Momin Siddiqui <156536928+SIDMINUL@users.noreply.github.com>
2026-09-13 16:58:19 +0530

    Add LangChain Ollama chatbot

---
+++ chatbotwithlama.py
@@ -0,0 +1,37 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms import Ollama
+
+import streamlit as st
+
+from dotenv import load_dotenv
+
+load_dotenv()
+# import os
+
+
+# os.environ["LANGCHAIN_TRACING_V2"] = os.getenv("LANGCHAIN_TRACING_V2")
+# os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+# os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGCHAIN_PROJECT")
+
+## Prompt Template
+
+prompt = ChatPromptTemplate(
+    [
+        ("system", "You are a helpful assistant. Please response to the user queries"),
+        ("user", "Question:{question}")
+    ]
+)
+
+## streamlit framework
+
+st.title("Langchain Demo with Olama")
+input_text = st.text_input("Search the topic you want")
+
+## Olama LLm
+llm = Ollama(model="llama2")
+output_parser = StrOutputParser()
+chain = prompt | llm | output_parser
+
+if input_text:
+    st.write(chain.invoke({"question": input_text}))
